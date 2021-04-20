c0c00a75647edd11df54127d2f8971eb1ef26ea8
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-20 18:39:26 +0200

    Aggiornato il funzionamento dello Script per quanto riguarda l'Header
    Adesso non solo i link sono generati in modo dinamico, ma anche il loro Href è generato seguendo il ciclo for; Ho reso dinamici anche gli altri link della barra nav che avevano un'icona come label. In aggiunta ho simulato il funzionamento della ricerca nella searchbar: alla pressione di invio il v-model salva il testo in una variabile che stampa in console, e svuota la barra di ricerca.

--- script/script.js
+++ script/script.js
@@ -4,14 +4,43 @@ const App = new Vue ({
   el: "#root",
 
   data: {
+
     mainNavLinks:[
-      "Home",
-      "Pages",
-      "Courses",
-      "Features",
-      "Blog",
-      "Shop",
-      
+
+      {
+        label:"Home",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(prima_stagione)"
+      },
+      {
+        label:"Pages",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(seconda_stagione)"
+      },
+      {
+        label:"Courses",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(terza_stagione)"
+      },
+      {
+        label:"Features",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(quarta_stagione)"
+      },
+      {
+        label:"Blog",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(quinta_stagione)"
+      },
+      {
+        label:"Shop",
+        url:"https://it.wikipedia.org/wiki/El_Camino_-_Il_film_di_Breaking_Bad"
+      },
+
+    ],
+
+    customerAreaLinks:[
+
+      {
+        label: "far fa-user-circle",
+        url: "https://it.wikipedia.org/wiki/Episodi_de_I_segreti_di_Twin_Peaks_(seconda_stagione)",
+      },
+
     ],
 
     searchInput:"",
@@ -19,6 +48,11 @@ const App = new Vue ({
   },
 
   methods: {
+
+    mySearch: function(){
+      console.log(this.searchInput);
+      this.searchInput="";
+    }
 
   },
 

8551b0b47b7612468cdc6d96afe3a0479d5e3612
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-04-22 10:56:30 +0200

    Footer completato. Script dei link del footer completato.

--- script/script.js
+++ script/script.js
@@ -43,7 +43,89 @@ const App = new Vue ({
 
     ],
 
+    FooterSocialLink:[
+
+      {
+        label:"fab fa-facebook-square",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(prima_stagione)",
+        description: "facebook"
+      },
+      {
+        label:"fab fa-twitter",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(seconda_stagione)",
+        description: "twitter"
+
+      },
+      {
+        label:"fab fa-instagram",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(terza_stagione)",
+        description: "instagram"
+      },
+      {
+        label:"fab fa-linkedin",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(terza_stagione)",
+        description: "linkedIn"
+      },
+
+    ],
+
+    footerLinkExploreA: [
+
+      {
+        label:"Start Here",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(quarta_stagione)"
+      },
+      {
+        label:"Blog",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(quinta_stagione)"
+      },
+      {
+        label:"About Us",
+        url:"https://it.wikipedia.org/wiki/El_Camino_-_Il_film_di_Breaking_Bad"
+      },
+
+    ],
+
+    footerLinkExploreB:[
+
+      {
+        label:"Success story",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(prima_stagione)"
+      },
+      {
+        label:"Courses",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(seconda_stagione)"
+      },
+      {
+        label:"Contact Us",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(terza_stagione)"
+      },
+
+    ],
+
+    footerLinkInfo:[
+
+      {
+        label:"Membership",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(prima_stagione)"
+      },
+      {
+        label:"Purchase guide",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(seconda_stagione)"
+      },
+      {
+        label:"Privacy policy",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(terza_stagione)"
+      },
+      {
+        label:"Terms of service",
+        url:"https://it.wikipedia.org/wiki/Episodi_di_Breaking_Bad_(prima_stagione)"
+      },
+
+    ],
+
     searchInput:"",
+
     emailInput:"",
 
   },
